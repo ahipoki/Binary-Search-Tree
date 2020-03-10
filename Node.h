54d621c9041dff19369f267841aafd9c1754f49c
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-09 18:24:04 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,4 +1,14 @@
+#ifndef NODE_H
+#define NODE_H
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
+
+class Node{
+  Node();
+  ~Node();
+  void setNext(Node*);
+  Node* getNext();
+}
