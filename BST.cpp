0d4741d5032771c822ed81dfa21391cbb0fa1959
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-18 20:18:53 -0700

    Create BST.cpp

---
+++ BST.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include "BST.h"
+
+using namespace std;
+
+Node* BST::Insert(Node* node, int key){
+  if (node == NULL){
+    node = new Node;
+    node->Key = key;
+    node->Left = NULL;
+    node->Right = NULL;
+    node->Parent = NULL;
+  }
+  else if (node->Key->Right = Insert(node->Right, key){
+    node->Right->Parent = node;
+  }
+  else{
+    node->Left = Insert(node->Left, key);
+    node->Left->Parent = node;
+  }
+  return node;
+}
