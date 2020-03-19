c41083c91288bfe774badb4aed3047300ef47ce4
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-18 20:27:35 -0700

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -4,43 +4,46 @@
 
 using namespace std;
 
-Node::Node(char* newValue){
-  value = new char[strlen(newValue)+1];
-  strcpy(value, newValue);
-  right = NULL;
-  left = NULL;
-  next = NULL;
+Node::Node(int newKey){
+  Key = new char[strlen(newKey)+1];
+  strcpy(Key, newKey);
+  Right = NULL;
+  Left = NULL;
+  Parent = NULL;
 }
 
-void Node::setNext(Node* newNext){
-  next = newNext;
+void Node::setKey(int newKey){
+  Key = newKey;
 }
 
-Node* Node::getNext(){
-  return next;
-}
-
-Node::~Node(){
-  delete[] value;
-  next = NULL;
-}
-
-char* Node::getValue(){
-  return value;
+int Node::getKey(){
+  return Key;
 }
 
 void Node::setLeft(Node* newLeft){
-  left = newLeft;
+  Left = newLeft;
 }
 
 Node* Node::getLeft(){
-  return left;
+  return Left;
 }
 
 void Node::setRight(Node* newRight){
-  right = newRight;
+  Right = newRight;
 }
 
 Node* Node::getRight(){
-  return right;
+  return Right;
+}
+
+Node* Node::setParent(Node* newParent){
+  Parent = newParent;
+}
+
+Node* Node::getParent(){
+  return Parent;
+}
+
+Node::~Node(){
+  
 }
