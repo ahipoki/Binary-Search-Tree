10e7167892594cfb70d752cf7bfe2a53ee406827
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-20 11:35:15 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -118,50 +118,11 @@ void strupper(char* str){
   }
 }
 
-void sort(int heap[], int len){
-  int originalLen = len;
-  for (int i = 0; i < originalLen; ++i){
-    cout<<heap[0]<<" ";
-    int last = heap[len-1];
-    heap[0] = last;
-    len--;
-    heapDown(heap, len, 0);
-  }
-}
-
-void printTree(int heap[], int len, int depth, int ind){
-  if (ind*2+1 < len){
-    printTree(heap, len, depth+1, ind*2+1);
-  }
-  for (int i = 0; i < depth; i++){
-    cout<<"     ";
-  }
-  cout<<heap[ind]<<endl;
-  if (ind*2+2 < len){
-    printTree(heap, len, depth+1, ind*2+2);
-  }
-}
-
-void heapDown(int heap[], int len, int in){
-  int largest = in;
-  int l = 2*in+1;
-  int r = 2*in+2;
-  if (l < len && heap[l] > heap[largest]){
-    largest = l;
-  }
-  if (r < len && heap[r] > heap[largest]){
-    largest = r;
-  }
-  if (largest != in){
-    swap(heap[in], heap[largest]);
-    heapDown(heap, len, largest);
-  }
-}
-
-void heapify(int heap[], int len){
-  int parent = len/2;
-  if (heap[len] > heap[parent] && len > 0){
-    swap(heap[len], heap[parent]);
-    heapify(heap, parent);
-  }
+void Insert(BST* bst){
+  int key;
+  cout<<"What number do you want to add to the tree?"<<endl;
+  cin>>key;
+  cin.clear();
+  cin.ignore(999, '\n');
+  bst->Insert(key);
 }
